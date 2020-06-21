9566235cc9d5a24079e5cb9756305fab610406be
dawsonwu1025 <dawson.wu@tatcs.com>
2020-06-20 22:47:02 -0400

    First commit

---
+++ maven-demo/src/test/java/TestHelloWorld.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testHelloWorld() {
+		System.out.print("...Hello World...!");
+		System.out.print("This is a test text!");
+	}
+
+}
